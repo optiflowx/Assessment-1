21381aaa45e790b7c222b17972dae26c2646e8ce
Prince <153428534+optiflowx@users.noreply.github.com>
2024-11-18 10:53:55 +0200

    initial commit

--- assess.py
+++ assess.py
@@ -5,15 +5,51 @@ def grade_average(grades):
     Args:
         grades (list): List of grades to calculate
     """
-
+    
+    if not all(isinstance(grade, int) for grade in grades):
+        return "Items in list must be integers"
+    
+    if len(grades) == 0:
+        return -1
+    
+    total = 0
+    neg_count = 0
+    
+    for grade in grades:
+        grade = int(grade)
+        
+        if grade < 0: neg_count += 1
+        
+        if grade > 0: total += grade
+        
+    divisor = len(grades) - neg_count
+        
+    if divisor == 0:
+        return total
+    else:
+        return total / divisor
 
 def find_prime_factors(number):
-    """Write code to return the prime factors of the number. 
-
-    Args:
-        number (int): Number to find the prime factors of
     """
+    Write code to return the prime factors of the number. 
 
+    Args: number (int): Number to find the prime factors of
+    """
+    def is_prime(num):
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+            
+        return True
+
+    prime_factors = []
+    
+    for i in range(2, number + 1):
+        if number % i == 0:
+            if is_prime(i):
+                prime_factors.append(i)
+            
+    return prime_factors
 
 def calculate_interest(principal, rate, years):
     """Write a program that returns the compound interest
@@ -23,7 +59,8 @@ def calculate_interest(principal, rate, years):
         rate (int): The interest rate
         years (int): The amount of years to calculate the interest for
     """
-
+    
+    return principal * (1 + rate) ** years
 
 def code_word(code):
     """Write a program that returns the word given a code.
@@ -34,7 +71,17 @@ def code_word(code):
     Args:
         code (list): The code to decipher
     """
-
+    
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    decoded = []
+    
+    for i in code:
+        if (i == 0):
+            decoded.append(" ")
+        else:
+            decoded.append(alphabet[i - 1])
+        
+    return "".join(decoded)
 
 def triangles(length):
     """Write a program that returns a triangle of a certain length
@@ -43,7 +90,7 @@ def triangles(length):
 
     Expected Output: 
     *
-    **
+    ** 
     ***
     ****
     *****
@@ -51,8 +98,17 @@ def triangles(length):
     Args:
         length (int): The ;ength your triangle should be
     """
-
-
+    
+    result = []
+    
+    for i in range(length):
+        result.append("*" * (i + 1))
+        
+        if i != length - 1:
+            result.append("\n")
+        
+    return "".join(result)
+        
 def hollow_triangle(length):
     """Write a program that returns a hollow triangle of a certain length
 
@@ -68,7 +124,19 @@ def hollow_triangle(length):
     Args:
         length (int): The ;ength your triangle should be
     """
-
+    
+    result = []
+    
+    for i in range(length):
+        if i == 0 or i == length - 1:
+            result.append("*" * (i + 1))
+        else:
+            result.append("*" + " " * (i - 1) + "*")
+            
+        if i != length - 1:
+            result.append("\n")
+        
+    return "".join(result)
 
 # There are no tests for this function so test by running the program. 
 def number_guessing(number):
@@ -85,4 +153,18 @@ def number_guessing(number):
     Args:
         number (int): The number to be guessed
     """
-
+    
+    is_correct = False
+    
+    for _ in range(2):
+        guess = int(input("Enter your guess: "))
+        
+        if guess == number:
+            print("Correct")
+            is_correct = True
+            break
+        else:
+            print("Incorrect")
+     
+    if not is_correct:    
+        print("Game Over: You have run out of guesses")
--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ from assess import *
 class TestCase(unittest.TestCase):
     
     def test_grade_ave(self):
-        self.assertIsInstance(grade_average([1,2,3,4,5]), str, "Items in list must be integers")
+        # self.assertIsInstance(grade_average([1,2,3,4,5]), str, "Items in list must be integers")
         self.assertEqual(grade_average([1,2,3,4,5]), 3, "Unexpected output")
         self.assertEqual(grade_average([46, 78, 32, -5, 6]), 40.5, "Incorrect output")
         self.assertEqual(grade_average([-56, 94, -3, 0]), 47, "Unexpected output")
@@ -13,10 +13,9 @@ class TestCase(unittest.TestCase):
 
 
     def test_primes(self):
-        self.assertIsInstance(find_prime_factors(5), int, "Expected a list")
+        # self.assertIsInstance(find_prime_factors(5), int, "Expected a list")
         self.assertEqual(find_prime_factors(3), [3], None)
         self.assertEqual(find_prime_factors(78), [2, 3, 13], "Unexpected output")    
-
 
     def test_interest(self):
         # write tests that cater for the output being an integer, unexpected outputs and expected ouputs
@@ -24,14 +23,14 @@ class TestCase(unittest.TestCase):
 
 
     def test_code_word(self):
-            self.assertIsInstance(code_word([3,1,20]), list, "Expected a string")
+            # self.assertIsInstance(code_word([3,1,20]), list, "Expected a string")
             self.assertEqual(code_word([16,25,20,8,15,14]), "python", "Unexpected output")
             self.assertEqual(code_word([14,1,20,21,18,5]), "nature", None)
             self.assertEqual(code_word([9,0,3,1,14,0,4,15,0,9,20]), "i can do it", "Unexpected output")
 
 
     def test_triangles(self):
-        self.assertIsInstance(triangles(5), str, "Expected a string")
+        # self.assertIsInstance(triangles(5), str, "Expected a string")
         self.assertEqual(triangles(3), "*\n**\n***", "Unexpected output")
         self.assertEqual(triangles(6), "*\n**\n***\n****\n*****\n******", "Unexpected output")
         self.assertEqual(triangles(7), "*\n**\n***\n****\n*****\n******\n*******", "Unexpected output")
